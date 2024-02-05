12e955508e975da9fb7603d2c4928dbead2c8ee2
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-02-05 14:18:13 -0300

    leis recursao

---
+++ dia-2/a_leis_recursao.py
@@ -0,0 +1,16 @@
+def countdown(n):
+    if n == 0: # caso base
+        print("FIM!")
+    else:
+        print(n)
+        countdown(n - 1) # caso recursivo
+
+countdown(5)
+
+def factorial(n):
+    if n <= 1: # caso base
+        return 1
+    else:
+        return n * factorial(n - 1) # caso recursivo
+    
+print(factorial(5))
