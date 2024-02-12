1cf3d03ac8476d614dd96178c97a8c019fe00a44
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-02-11 23:36:58 -0300

    salvando

--- dia-3/e_algoritmos_dividir_conquistar.py
+++ dia-3/e_algoritmos_dividir_conquistar.py
@@ -31,5 +31,5 @@ def merge(numbers, start, mid, end):
 
 
 numbers = [6, 5, 3, 1, 8, 7, 2, 4]
-merge_sort(numbers, 0, len(numbers))
-print(numbers)
+# merge_sort(numbers, 0, len(numbers))
+# print(numbers)
